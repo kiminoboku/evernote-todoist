8ccba4718261349863d75a966a9c8974d67ba899
Radosław Lewandowski <kiminoboku@gmail.com>
2021-09-04 01:25:56 +0200

    Cleanup

--- src/main/java/pl/kiminoboku/EvernoteAuthData.java
+++
@@ -1,6 +0,0 @@
-package pl.kiminoboku;
-
-public class EvernoteAuthData {
-    static final String AUTH_TOKEN = "S=s1:U=96758:E=182d7222bc9:C=17b7f70ff48:P=1cd:A=en-devtoken:V=2:H=589e71566dafcac4f23de6704952196a";
-    static final String NOTE_STORE_URL = "https://sandbox.evernote.com/shard/s1/notestore";
-}
--- src/main/java/pl/kiminoboku/RootHandler.java
+++ src/main/java/pl/kiminoboku/RootHandler.java
@@ -4,22 +4,13 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
-import io.vavr.collection.HashSet;
-import io.vavr.collection.Set;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.core.ResolvableType;
 import pl.kiminoboku.spring.LambdaContextConfiguration;
 import pl.kiminoboku.spring.SpringSupport;
 
-import java.util.UUID;
-
 
 public class RootHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
-    private static final Set<UUID> NOTEBOOK_UUIDS = HashSet.of(
-            UUID.fromString("50d41f65-02ec-7f28-d144-c5df7b04ceaa"), //Dom
-            UUID.fromString("41bb3166-1846-0439-d1be-21cfcc1745eb"), //Mermet
-            UUID.fromString("13e549ce-e958-4854-3094-31fc275e3ae9") //Praca
-    );
 
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent input, Context context) {
